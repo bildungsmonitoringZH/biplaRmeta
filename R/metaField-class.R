aa879d687f95461df8fae43a4e3e9dfd24c55c63
Flavian Imlig <flavian.imlig@bi.zh.ch>
2020-05-26 17:01:04 +0200

    R/metaField-class.R - new method $print(), changes in methods $initialize(), new aux functions listDefLanguage(), listOutput()

--- R/metaField-class.R
+++ R/metaField-class.R
@@ -1,55 +1,73 @@
 # class for meta fields
 #
 # Author: Flavian Imlig <flavian.imlig@bi.zh.ch>
-# Date: 25.05.2020
+# Date: 26.05.2020
 ###############################################################################
 
 #' R6 class representing the information of one
-MetaField <- R6::R6Class(classname = 'MetaField',
-                         public = list(
-                             #' @description
-                             #' Create a MetaField object
-                             #' @param name (string) Name of the field
-                             #' @param data Value of the field
-                             #' @examples
-                             #' f <- MetaField$new(name = 'source_url', data = 'https://bi.zh.ch/bildungsplanung')
-                             #' f$list
-                             #' @return A new `MetaField` object.
-                             initialize = function(name, data)
-                             {
-                                 # parse arguments
-                                 assert_that(is.string(name))
-                                 assert_that(noNA(name))
-                                 assert_that(name %in% fields$field)
+MetaField <- R6::R6Class(
+    classname = 'MetaField',
+    public = list(
+        #' @description
+        #' Create a MetaField object
+        #' @param name (string) Name of the field
+        #' @param data Value of the field
+        #' @examples
+        #' f <- MetaField$new(name = 'source_url', data = 'https://bi.zh.ch/bildungsplanung')
+        #' f$list
+        #' @return A new `MetaField` object.
+        initialize = function(name, data)
+        {
+            # parse arguments
+            assert_that(is.string(name))
+            assert_that(noNA(name))
+            assert_that(name %in% fields$field, msg = sprintf('\'%s\' is not a valid MetaField name.', name))
 
-                                 idx <- which(fields$field == name)
-                                 private$depth <- fields$depth[idx]
-                                 private$class <- fields$class[idx]
+            idx <- which(fields$field == name)
+            private$depth <- fields$depth[idx]
+            private$class <- fields$class[idx]
 
-                                 assert_that(listDepth(list(data)) <= private$depth, msg = sprintf('Data argument with depth <= %d expected for field %s.', private$depth, name))
-                                 private$depth <- listDepth(list(data))
-                                 assert_that(private$class %in% listClass(list(data)), msg = sprintf('Data argument with class %s expected for field %s.', private$class, name))
+            data_depth <- listDepth(list(data))
+            data_class <- listClass(list(data))
 
-                                 # assign
-                                 self$data <- data
-                                 self$name <- name
-                             },
-                             #' @field name Actual name of the field
-                             name = 'none',
-                             #' @field data Actual data of the field
-                             data = list()
-                             ),
-                         private = list(
-                             # @field depth Max depth of field
-                             depth = 0,
-                             # @field class Class of meta information
-                             class = 'none'
-                         ),
-                         active = list(
-                             #' @field list The data of a MetaField in list format
-                             list = function() { setNames(object = list(self$data), self$name) }
-                         )
+            assert_that(data_depth <= private$depth, msg = sprintf('Data argument with depth <= %d expected for field %s.', private$depth, name))
+            private$depth <- data_depth
 
+            if( private$class == 'LanguageString' & 'character' %in% data_class) data <- listDefLanguage(data)
+            assert_that(private$class %in% listClass(list(data)), msg = sprintf('Data argument with class %s expected for field %s.', private$class, name))
+
+            # assign
+            self$data <- data
+            self$name <- name
+        },
+        #' @description
+        #' Create a text representation
+        #' @return string
+        print = function()
+        {
+            str_oneline <- sprintf('%s: list with class %s and depth %.0f', self$name, private$class, private$depth)
+            cat(str_oneline, "\n")
+            invisible(self)
+        },
+        #' @field name Actual name of the field
+        name = 'none',
+        #' @field data Actual data of the field
+        data = list()
+    ),
+    private = list(
+        # @field depth Max depth of field
+        depth = 0,
+        # @field class Class of meta information
+        class = 'none'
+    ),
+    active = list(
+        #' @field list The data of a MetaField in list format
+        list = function() {
+            list_raw <- list(self$data)
+            names(list_raw) <- self$name
+            output <- listOutput(list_raw)
+        }
+    )
 )
 
 # aux functions
@@ -85,6 +103,32 @@ listClass <- function(this)
     }
 }
 
+#' @rdname listDepth
+#' @keywords internal
+#' @return character
+listDefLanguage <- function(this)
+{
+    if( is.character(this) )
+    {
+        ls <- do.call(LanguageString$new, args = setNames(list(this), as.character(langs$code[1])))
+        return(ls)
+    } else if(is.list(this) ) {
+        return(lapply(this, listDefLanguage))
+    } else {
+        return(this)
+    }
+}
 
-# TODO next: Wrapper für einfache Eingabe von LanguageString$new, siehe test-MetaField.R:26
+listOutput <- function(this)
+{
+    if( is.list(this) )
+    {
+        return(lapply(this, listOutput))
+    } else if( is(this, 'LanguageString') ) {
+        return(this$list)
+    } else {
+        return(this)
+    }
+}
+
 # a <- jsonlite::read_json('C:\\Users\\B251PFI\\Documents\\Rworkspace\\swissdata_demo\\data\\ch_adecco_sjmi.json')
--- tests/testthat/test-MetaField.R
+++ tests/testthat/test-MetaField.R
@@ -1,30 +1,51 @@
 # test MetaField-class
 #
 # Author: Flavian Imlig <flavian.imlig@bi.zh.ch>
-# Date: 25.05.2020
+# Date: 26.05.2020
 ###############################################################################
 
 context('test MetaField-class')
 
-test_that("MetaField$new(), return value", {
-  a <- MetaField$new(name = 'title', data = LanguageString$new(de = 'Titel'))
-  b <- MetaField$new(name = 'source_name', data = LanguageString$new(de = 'Bildungsplanung Kanton Zürich'))
-  c <- MetaField$new(name = 'source_url', data = 'https://bi.zh.ch/bildungsplanung')
-  d <- MetaField$new(name = 'issued', data = lubridate::ymd_hm('2020-05-25 2023'))
-  e <- MetaField$new(name = 'modified', data = Sys.time())
-  f <- MetaField$new(name = 'units', data = list('all' = LanguageString$new(en = 'Count')))
-  g <- MetaField$new(name = 'aggregate', data = list(m1 = 'mean', m2 = 'sum'))
-  h <- MetaField$new(name = 'dim_order', data = c('m1', 'm3', 'm2'))
-  i <- MetaField$new(name = 'hierarchy', data = list(ma = 'm1', mb = list('m2', mc = 'm3')))
-  j <- MetaField$new(name = 'labels', data = list(dimnames = list(m1 = LanguageString$new(de = 'erstes Merkmal'),
-                                                                  m2 = LanguageString$new(en = 'second feature')),
-                                                  m1 = list(v1 = LanguageString$new(de = 'erster Wert des ersten Merkmals'))))
-  k <- MetaField$new(name = 'description', data = list(all = LanguageString$new(de = 'Beschreibung des ganzen Datensatzes'),
-                                                       dim = list(m1 = LanguageString$new(en = 'Description of the first feature'),
-                                                                  m2 = LanguageString$new(fr = 'Description de la deuxieme caracteristique')),
-                                                       m1 = list(v1 = LanguageString$new(de = c('Beschreibung eines Wertes von m1', 'Hier muss man besonders aufpassen')))))
-  l <- MetaField$new(name = 'description', data = list(all = 'Beschreibung des ganzen Datensatzes',
-                                                       dim = list(m1 = 'Beschreibung des ersten Merkmals',
-                                                                  m2 = 'Beschreibung des ersten Merkmals'),
-                                                       m1 = list(v1 = c('Beschreibung eines Wertes von m1', 'Hier muss man besonders aufpassen'))))
+test_that("MetaField$new(), argument parsing", {
+    expect_error(MetaField$new(name = 22), regexp = 'name is not a string')
+    expect_error(MetaField$new(name = letters[1:5]), regexp = 'name is not a string')
+    expect_error(MetaField$new(name = NA_character_), regexp = 'name contains . missing values')
+    expect_error(MetaField$new(name = 'hubi'), regexp = 'is not a valid MetaField name')
+
+    expect_error(MetaField$new(name = 'title', data = list(a = list(b = 'c'))), regexp = 'argument with depth <= 1 expected for field title')
+    expect_error(MetaField$new(name = 'labels', data = list(a = list(b = list(c = list(d = 'e'))))), regexp = 'argument with depth <= 3 expected for field labels')
+    expect_error(MetaField$new(name = 'title', data = 22), regexp = 'argument with class LanguageString expected for field title')
+    expect_error(MetaField$new(name = 'issued', data = 'hubi'), regexp = 'argument with class POSIXct expected for field issued')
 })
+
+test_that("MetaField$new(), return value", {
+    res <- list()
+    res$a <- MetaField$new(name = 'title', data = LanguageString$new(de = 'Titel'))
+    res$b <- MetaField$new(name = 'source_name', data = LanguageString$new(de = 'Bildungsplanung Kanton Zürich'))
+    res$c <- MetaField$new(name = 'source_url', data = 'https://bi.zh.ch/bildungsplanung')
+    res$d <- MetaField$new(name = 'issued', data = lubridate::ymd_hm('2020-05-25 2023'))
+    res$e <- MetaField$new(name = 'modified', data = Sys.time())
+    res$f <- MetaField$new(name = 'units', data = list('all' = LanguageString$new(en = 'Count')))
+    res$g <- MetaField$new(name = 'aggregate', data = list(m1 = 'mean', m2 = 'sum'))
+    res$h <- MetaField$new(name = 'dim_order', data = c('m1', 'm3', 'm2'))
+    res$i <- MetaField$new(name = 'hierarchy', data = list(ma = 'm1', mb = list('m2', mc = 'm3')))
+    res$j <- MetaField$new(name = 'labels', data = list(dimnames = list(m1 = LanguageString$new(de = 'erstes Merkmal'),
+                                                                    m2 = LanguageString$new(en = 'second feature')),
+                                                    m1 = list(v1 = LanguageString$new(de = 'erster Wert des ersten Merkmals'))))
+    res$k <- MetaField$new(name = 'description', data = list(all = LanguageString$new(de = 'Beschreibung des ganzen Datensatzes'),
+                                                         dim = list(m1 = LanguageString$new(en = 'Description of the first feature'),
+                                                                    m2 = LanguageString$new(fr = 'Description de la deuxieme caracteristique')),
+                                                         m1 = list(v1 = LanguageString$new(de = c('Beschreibung eines Wertes von m1', 'Hier muss man besonders aufpassen')))))
+    res$l <- MetaField$new(name = 'description', data = list(all = 'Beschreibung des ganzen Datensatzes',
+                                                         dim = list(m1 = 'Beschreibung des ersten Merkmals',
+                                                                    m2 = 'Beschreibung des ersten Merkmals'),
+                                                         m1 = list(v1 = c('Beschreibung eines Wertes von m1', 'Hier muss man besonders aufpassen'))))
+    purrr::walk(res, ~expect_is(.x, 'MetaField'))
+    expect_identical(unname(purrr::map_chr(res, ~.x$name)), levels(fields$field)[c(seq_along(levels(fields$field)), nlevels(fields$field))])
+    purrr::walk(res, ~expect_is(.x$list, 'list'))
+    expect_identical(unname(purrr::map_chr(res, ~listClass(.x$data) %>% purrr::keep(~.x %in% fields$class))), as.character(fields$class)[c(seq(1, nrow(fields)), nrow(fields))])
+    expect_identical(unname(purrr::map_dbl(res, ~listDepth(.x$data)) + 1), c(1,1,1,1,1,2,2,1,3,3,3,3))
+})
+
+tbl <- mtcars
+attr(tbl, 'meta') <- res$a
